692e26c2c8fe4e903104aa60e65865cc97699539
Stephanie <stephanieorazem@jovensgenios.com>
2021-09-15 22:04:55 -0300

    :sparkles: product entry

---
+++ src/components/ReceiveProduct.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="pa-4">
+    <h2>Entrada de Produto</h2>
+    <v-form class="mt-8 pa-4" v-model="isValid">
+      <v-autocomplete
+        v-model="productCode"
+        :items="productsCodes"
+        label="Código do produto"
+        required
+        :rules="[v => !!v || 'Escolha o código do produto']"
+      />
+
+      <v-text-field
+        v-model="price"
+        label="Valor unitário do produto"
+        type="number"
+        :rules="[v => !!v || 'Preencha o valor de venda']"
+        required
+      />
+
+      <v-text-field
+        v-model="quantity"
+        label="Quantidade movimentada"
+        type="number"
+        :rules="[v => !!v || 'Preencha a quantidade movimentada']"
+        required
+      />
+
+      <v-btn
+        class="mt-4 elevation-1 white--text"
+        :disabled="!isValid"
+        color="#7F6F62"
+        @click="submit"
+      >
+        Enviar
+      </v-btn>
+
+    </v-form>
+    <v-snackbar v-model="alertSuccess" rounded timeout="5000" top color="success">
+      <div class="d-flex align-center justify-space-between">
+        <span>Entrada efetuada com sucesso!</span>
+        <v-icon @click="alertSuccess=false">mdi-close</v-icon>
+      </div>
+    </v-snackbar>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Entrada',
+  data(){
+    return{
+      productCode: '',
+      price: undefined,
+      quantity: undefined,
+      isValid: false,
+      alertSuccess: false
+    }
+  },
+  computed:{
+    productsCodes(){
+      return this.$store.state.products.map( item => item.code )
+    }
+  },
+  methods:{
+    submit(){
+      this.$store.commit('receiveProduct', {
+        productCode: this.productCode,
+        type: 'in',
+        quantity: Number(this.quantity),
+        price: Number(this.price)
+      })
+
+      this.alertSuccess = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,39 @@ const store = new Vuex.Store({
   state: {
     products: products,
     stockTransactions: stockTransactions
+  },
+
+  mutations: {
+    receiveProduct(state, payload){
+      state.stockTransactions.push({
+        product: payload.productCode,
+        type: payload.type,
+        price: payload.price,
+        quantity: payload.quantity
+      })
+
+      let index = state.products.findIndex(
+        item => item.code === payload.productCode
+      )
+
+      state.products[index].quantity += payload.quantity
+    },
+
+    exitProduct(state, payload){
+      let index = state.products.findIndex(
+        item => item.code === payload.productCode
+      )
+
+      state.products[index].quantity -= payload.quantity
+
+      state.stockTransactions.push({
+        product: payload.productCode,
+        type: payload.type,
+        price: state.products[index].price,
+        quantity: payload.quantity
+      })
+
+    }
   }
 })
 
